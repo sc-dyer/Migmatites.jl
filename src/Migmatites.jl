d9ffd557dcbf692f887c8f56aba224b3849114e4
scdyer <scdyer@uwaterloo.ca>
2024-10-31 14:31:21 -0400

    Added close system equilibrate

--- src/Migmatites.jl
+++ src/Migmatites.jl
@@ -2,7 +2,9 @@ module Migmatites
 
 export
     getmelt,
-    equilibrate_open_system
+    equilibrate_open_system,
+    equilibrate_closed_system,
+    balance_component
 
 using
     DocStringExtensions,
@@ -20,7 +22,7 @@ using
 """
 $(TYPEDSIGNATURES)
 
-Calculates the phase equilibria at specified T and P conditions for given composition and
+Calculates the phase equilibria at specified T and P conditions for given composition and 
 returns the melt phase in the calculated system. Must run init_meemum before running this function.
 """
 function getmelt(composition,T,P; suppresswarn = false)
@@ -31,7 +33,7 @@ end
 """
 $(TYPEDSIGNATURES)
 
-Returns the melt phase in a 'PetroSystem' if one exists, returns an empty phase with 0 mols of every component
+Returns the melt phase in a 'PetroSystem' if one exists, returns an empty phase with 0 mols of every component 
 if no melt exists
 """
 function getmelt(system)
@@ -41,17 +43,14 @@ end
 """
 $(TYPEDSIGNATURES)
 
-This is a two step function which calculates the system and phase properties of the melt source rock at 'source_T' and
-'source_P', then use the chemical potential of `equilib_component` to calculate the system and phase properties of
-the host rock at `host_T` and `host_P`. If `source_compo` or `host_compo` are not provided, it will default to
-the compositions defined in the given dat files: `sourcefile` and `hostfile`. For this function to work as expected
+This is a two step function which calculates the system and phase properties of the melt source rock at 'source_T' and 
+'source_P', then use the chemical potential of `equilib_component` to calculate the system and phase properties of 
+the host rock at `host_T` and `host_P`. If `source_compo` or `host_compo` are not provided, it will default to 
+the compositions defined in the given dat files: `sourcefile` and `hostfile`. For this function to work as expected 
 your sourcefile must have only P and T as independent variables, and your hostfile must have P, T and μ of 
 one component as independent variables.
 """
 function equilibrate_open_system(sourcefile,hostfile,source_T,source_P, host_T,host_P;source_compo = nothing, host_compo = nothing,equilib_component = "H2O",suppresswarn = false, phasefunc = [])
-    #Should define another function for multiple compositions at source
-    #So that init_meemum does not need to be run a billion times
-
     #Need to run these in sequence,first calc melt system at T and P
     #Then get uH2O from that melt and calc host system for given uH2O
     #Complicated by how datafiles are defined
@@ -92,7 +91,7 @@ end
 """
 $(TYPEDSIGNATURES)
 
-This does the same as the "single point" version of this function but provides a range of output for
+This does the same as the "single point" version of this function but provides a range of output for 
 variable source compositions on a linear range between 'source_compo_start' and 'source_compo_end'.
 """
 function equilibrate_open_system(sourcefile, hostfile,source_T,source_P, host_T,host_P, source_compo_start, source_compo_end; host_compo = nothing,equilib_component = "H2O",suppresswarn = false, steps = 100, phasefunc = [])
@@ -161,7 +160,7 @@ end
 """
 $(TYPEDSIGNATURES)
 
-This does the same as the "single point" version of this function but provides a range of output for
+This does the same as the "single point" version of this function but provides a range of output for 
 variable source compositions on a linear range between 'source_T1' and 'source_T2'.
 """
 function equilibrate_open_system(sourcefile, hostfile,source_T1, source_T2, source_P, host_T,host_P;source_compo = nothing, host_compo = nothing,equilib_component = "H2O",suppresswarn = false, steps = 100, phasefunc = [])
@@ -231,4 +230,115 @@ function equilibrate_open_system(sourcefile, hostfile,source_T1, source_T2, sour
 
 end
 
+"""
+$(TYPEDSIGNATURES)
+
+This is a two step function which calculates the system and phase properties of the melt source rock at 'source_T' and
+'source_P', then use the chemical potential of `equilib_component` and `melt_proportion` to calculate the system and 
+phase properties of the host rock at `host_T` and `host_P` attempts to converge on a proportion of the component of 
+interest where both melt and host have the same chemical potential in that component. `melt_proportion` is the molar ratio 
+of melt to host rock. If `source_compo` or `host_compo` are not provided, it will default to the compositions defined 
+in the given dat files: `sourcefile` and `hostfile`. For this function to work as expected your sourcefile and 
+hostfile must have only P and T as independent variables and they must contain the same components.
+"""
+function equilibrate_closed_system(meemumlib,sourcecompo,hostcompo,source_T,source_P, host_T,host_P, melt_proportion;equilib_component = "H2O",suppresswarn = false, phasefunc = [],μthreshold = 2.0,xthreshold = 1E-5)
+    #Set up: get the host rock composition
+   
+    source_system = minimizepoint(meemumlib,source_T,source_P,suppresswarn=suppresswarn, composition = sourcecompo, phasefunc = phasefunc)
+   
+    melt = getmelt(source_system)
+    if mol(melt) ≈ 0
+        println("No melt generated at ",source_T," °C and ", source_P, " bar" )
+        return
+    end
+
+    melt_system = minimizepoint(meemumlib,host_T,host_P,composition = melt.composition.*100, suppresswarn = suppresswarn, phasefunc = phasefunc)
+    host_system = minimizepoint(meemumlib,host_T,host_P,composition = hostcompo, suppresswarn = suppresswarn, phasefunc = phasefunc)
+
+    μ_melt = melt_system.composition[findchemical(melt_system.composition,equilib_component)].μ
+    μ_host = host_system.composition[findchemical(host_system.composition,equilib_component)].μ
+
+    if μ_melt < μ_host
+        println("Host μ is higher than melt μ")
+        return source_system,melt_system, host_system
+    end
+    
+    
+    count = 1
+    
+    x_change = 0.001
+    if molfrac(melt_system.composition,equilib_component) > 0.15
+        x_change = 0.01
+    end
+    # melt_system, host_system = recursive_equilibrate(sourcelib,melt.composition,hcomp,melt_proportion,host_T, host_P,equilib_component=equilib_component,suppresswarn=suppresswarn,phasefunc=phasefunc)
+    while abs(μ_melt - μ_host) >μthreshold && molfrac(melt_system.composition,equilib_component) > xthreshold && abs(x_change) > 1e-8
+
+        # x_change = molfrac(melt_system.composition,equilib_component)/divisor
+        @show x_change
+        newmeltcompo, newhostcompo = balance_component(melt_system.composition,host_system.composition,equilib_component,x_change,melt_proportion)
+        newmelt_system = minimizepoint(meemumlib,host_T,host_P,composition = newmeltcompo, suppresswarn = suppresswarn, phasefunc = phasefunc)
+        newhost_system = minimizepoint(meemumlib,host_T,host_P,composition = newhostcompo, suppresswarn = suppresswarn, phasefunc = phasefunc)
+    
+        nμ_melt = newmelt_system.composition[findchemical(newmelt_system.composition,equilib_component)].μ
+        nμ_host = newhost_system.composition[findchemical(newhost_system.composition,equilib_component)].μ
+        
+        if (nμ_melt - nμ_host)/x_change < 0
+            x_change /= -10
+        end
+        melt_system = newmelt_system
+        host_system = newhost_system
+        μ_melt = nμ_melt
+        μ_host = nμ_host
+       
+        Xh2o_melt = molfrac(melt_system.composition,equilib_component)
+        Xh2o_host = molfrac(host_system.composition,equilib_component)
+        println("Loop ", count)
+        @show μ_host, μ_melt
+        @show Xh2o_host, Xh2o_melt
+        count += 1
+    end
+    return source_system,melt_system, host_system
+
+end
+
+function recursive_equilibrate(meemumlib, meltcompo,hostcompo, melt_proportion, T, P;equilib_component = "H2O",threshold = 0.5, suppresswarn = false, phasefunc = [])
+
+    melt_system = minimizepoint(meemumlib,T,P,composition = meltcompo, suppresswarn = suppresswarn, phasefunc = phasefunc)
+    host_system = minimizepoint(meemumlib,T,P,composition = hostcompo, suppresswarn = suppresswarn, phasefunc = phasefunc)
+
+    μ_melt = melt_system.composition[findchemical(melt_system.composition,equilib_component)].μ
+    μ_host = host_system.composition[findchemical(host_system.composition,equilib_component)].μ
+
+    if abs(μ_melt - μ_host) < threshold
+        return melt_system, host_system
+    end
+
+    x_change = (molfrac(melt_system.composition,equilib_component) - molfrac(host_system.composition,equilib_component))/10
+    newmeltcompo, newhostcompo = balance_component(melt_system,host_system,equilib_component,x_change,melt_proportion)
+
+    return recursive_equilibrate(meemumlib,newmeltcompo,newhostcompo,melt_proportion,T,P,equilib_component=equilib_component,threshold=threshold,suppresswarn=suppresswarn,phasefunc=phasefunc)
+end
+
+function balance_component(compo1, compo2, component,diff, proportion)
+    #Takes from system1 and adds to system2 according to proportion of system1 to system2
+    #So if there is 2 times as much of system1 as system2, and system1 is losing 1% of its H2O 
+    #to system2, system2 will gain twice as much of the H2O
+    
+    
+
+    #Normalize to 1 mol
+    x1 = molfrac(compo1,component)
+    x2 = molfrac(compo2,component)
+
+    x1 = x1 - diff
+    x2 = x2 + diff*proportion
+    #Reverse molfrac normalization
+    mol1 = x1*sum_mols(compo1)
+    mol2 = x2*sum_mols(compo2)
+    compo1 = change_list_component(compo1,mol1,component)
+    compo2 = change_list_component(compo2,mol2,component)
+
+    return compo1, compo2
+    #Get proportion of component of interestt
+end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,175 +6,194 @@ using JPerpleX
 
 @testset "Migmatites.jl" begin
     #Test1
-    hostlib = init_meemum("23SD20A_melt-test1/Host")
-    host = minimizepoint(hostlib,800,9000,μ1 = -316240)
+    @testset "Open system tests" begin
+        hostlib = init_meemum("23SD20A_melt-test1/Host")
+        host = minimizepoint(hostlib,800,9000,μ1 = -316240)
 
-    hosth2o = getchemical(host.composition,"H2O")
+        hosth2o = getchemical(host.composition,"H2O")
 
-    @test round(hosth2o.mol,digits=3) ≈ 0.414
-    @test round(hosth2o.μ,sigdigits = 6) ≈ -316240
-    @test round(host.phases[1].vol/host.vol*100,digits=2) ≈ 5.15
-    close_meemum!(hostlib)
+        @test round(hosth2o.mol,digits=3) ≈ 0.414
+        @test round(hosth2o.μ,sigdigits = 6) ≈ -316240
+        @test round(host.phases[1].vol/host.vol*100,digits=2) ≈ 5.15
+        close_meemum!(hostlib)
 
-    source, melt, host = equilibrate_open_system("23SD20A_melt-test1/MeltSource","23SD20A_melt-test1/Host",875,10000,800,9000)
+        source, melt, host = equilibrate_open_system("23SD20A_melt-test1/MeltSource","23SD20A_melt-test1/Host",875,10000,800,9000)
 
-    sourcemelt = getmelt(source)
-    sourcemelth2o= getchemical(sourcemelt.composition,"H2O")
-    @test round(sourcemelth2o.mol,digits=5) ≈ 0.40998
-    @test round(sourcemelt.vol/source.vol * 100,digits=2) ≈ 6.89
-    # @test sourcemelt.composition .≈ melt.composition
+        sourcemelt = getmelt(source)
+        sourcemelth2o= getchemical(sourcemelt.composition,"H2O")
+        @test round(sourcemelth2o.mol,digits=5) ≈ 0.40998
+        @test round(sourcemelt.vol/source.vol * 100,digits=2) ≈ 6.89
+        # @test sourcemelt.composition .≈ melt.composition
 
-    melth2o = getchemical(melt.composition,"H2O")
-    @test round(melth2o.μ,sigdigits=6) ≈ -316239
-    @test round(melth2o.mol,digits=3) ≈ 40.998
+        melth2o = getchemical(melt.composition,"H2O")
+        @test round(melth2o.μ,sigdigits=6) ≈ -316239
+        @test round(melth2o.mol,digits=3) ≈ 40.998
+        
+        hosth2o = getchemical(host.composition,"H2O")
+
+        @test round(hosth2o.mol,digits=3) ≈ 0.414
+        @test round(hosth2o.μ,sigdigits = 6) ≈ -316239
+        @test round(host.phases[1].vol/host.vol*100,digits=2) ≈ 5.15
+
+        #Test2
+        source, melt, host = equilibrate_open_system("23SD20A_melt-test2/MeltSource","23SD20A_melt-test2/Host",875,10000,800,9000)
+
+        sourcemelt = getmelt(source)
+        sourcemelth2o= getchemical(sourcemelt.composition,"H2O")
+        @test round(sourcemelth2o.mol,digits=5) ≈ 0.67574
+        @test round(sourcemelt.vol/source.vol * 100,digits=2) ≈ 97.78
+        # @test sourcemelt.composition .≈ melt.composition
+
+        melth2o = getchemical(melt.composition,"H2O")
+        @test round(melth2o.μ,sigdigits=6) ≈ -311522
+        @test round(melth2o.mol,digits=3) ≈ 67.574
+        
+        hosth2o = getchemical(host.composition,"H2O")
+
+        @test round(hosth2o.mol,digits=3) ≈ 47.143
+        @test round(hosth2o.μ,sigdigits=6) ≈ -311522
+        hostmelt = getmelt(host)
+        @test round(hostmelt.vol/host.vol*100,digits=2) ≈ 96.07
+        hostmelth2o = getchemical(hostmelt.composition,"H2O")
+        @test round(hostmelth2o.mol,digits=2) ≈ 0.67
+
+
+
+        sourcelib = init_meemum("23SD20A_melt-test1/MeltSource")
+        source_compo = getcompo(sourcelib)
+        close_meemum!(sourcelib)
+
+        h2ostart = 1.0
+        h2oend = 50.0
+        
+        source_compo1 = change_list_component(source_compo,h2ostart,"H2O")
+        source_compo2 = change_list_component(source_compo,h2oend,"H2O")
     
-    hosth2o = getchemical(host.composition,"H2O")
+        sources, melts, hosts = equilibrate_open_system("23SD20A_melt-test1/MeltSource","23SD20A_melt-test1/Host",875,10000,800,9000,source_compo1,source_compo2, steps =10)
 
-    @test round(hosth2o.mol,digits=3) ≈ 0.414
-    @test round(hosth2o.μ,sigdigits = 6) ≈ -316239
-    @test round(host.phases[1].vol/host.vol*100,digits=2) ≈ 5.15
+        source = sources[1]
+        melt = melts[1]
+        host = hosts[1]
+        sourcemelt = getmelt(source)
+        sourcemelth2o= getchemical(sourcemelt.composition,"H2O")
+        @test round(sourcemelth2o.mol,digits=5) ≈ 0.40998
+        @test round(sourcemelt.vol/source.vol * 100,digits=2) ≈ 6.89
+        # @test sourcemelt.composition .≈ melt.composition
 
-    #Test2
-    source, melt, host = equilibrate_open_system("23SD20A_melt-test2/MeltSource","23SD20A_melt-test2/Host",875,10000,800,9000)
+        melth2o = getchemical(melt.composition,"H2O")
+        @test round(melth2o.μ,sigdigits=6) ≈ -316239
+        @test round(melth2o.mol,digits=3) ≈ 40.998
+        
+        hosth2o = getchemical(host.composition,"H2O")
 
-    sourcemelt = getmelt(source)
-    sourcemelth2o= getchemical(sourcemelt.composition,"H2O")
-    @test round(sourcemelth2o.mol,digits=5) ≈ 0.67574
-    @test round(sourcemelt.vol/source.vol * 100,digits=2) ≈ 97.78
-    # @test sourcemelt.composition .≈ melt.composition
+        @test round(hosth2o.mol,digits=3) ≈ 0.414
+        @test round(hosth2o.μ,sigdigits = 6) ≈ -316239
+        @test round(host.phases[1].vol/host.vol*100,digits=2) ≈ 5.15
 
-    melth2o = getchemical(melt.composition,"H2O")
-    @test round(melth2o.μ,sigdigits=6) ≈ -311522
-    @test round(melth2o.mol,digits=3) ≈ 67.574
+        source = sources[10]
+        melt = melts[10]
+        host = hosts[10]
+        sourcemelt = getmelt(source)
+        sourcemelth2o= getchemical(sourcemelt.composition,"H2O")
+        @test round(sourcemelth2o.mol,digits=5) ≈ 0.67574
+        @test round(sourcemelt.vol/source.vol * 100,digits=2) ≈ 97.78
+        # @test sourcemelt.composition .≈ melt.composition
+
+        melth2o = getchemical(melt.composition,"H2O")
+        @test round(melth2o.μ,sigdigits=6) ≈ -311522
+        @test round(melth2o.mol,digits=3) ≈ 67.574
+        
+        hosth2o = getchemical(host.composition,"H2O")
+
+        @test round(hosth2o.mol,digits=3) ≈ 47.143
+        @test round(hosth2o.μ,sigdigits=6) ≈ -311522
+        hostmelt = getmelt(host)
+        @test round(hostmelt.vol/host.vol*100,digits=2) ≈ 96.07
+        hostmelth2o = getchemical(hostmelt.composition,"H2O")
+        @test round(hostmelth2o.mol,digits=2) ≈ 0.67
+
+        fig = Figure(size = (600,450))
+        ax = Axis(fig[1,1])
+        phasemode!(ax,range(h2ostart,h2oend,10),hosts)
+        fig[1,2] = Legend(fig,ax)
+        save("23SD20A_melt-test1/Host.svg",fig)
+
+    end
+
+    @testset "Closed system tests" begin
+
+        hostlib = init_meemum("23SD20A_melt-test3/Host")
+        hostcompo = hostlib.composition
+        close_meemum!(hostlib)
+
+        sourcelib = init_meemum("23SD20A_melt-test3/MeltSource")
+        sourcecompo = sourcelib.composition
+        
+
+        compo1, compo2 = balance_component(sourcecompo,hostcompo,"H2O",0.01,2.0)
+
+        @test round(molfrac(compo1,"H2O"),sigdigits = 3) ≈ 0.0833
+        @test round(molfrac(compo2,"H2O"),sigdigits = 3) ≈ 0.0256
+
+        source, melt, host = equilibrate_closed_system(sourcelib,sourcecompo,hostcompo,875,10000,800,9000,2.0)
+        
+        sourcemelt = getmelt(source)
+        sourcemelth2o= getchemical(sourcemelt.composition,"H2O")
+        @test round(sourcemelth2o.mol,digits=5) ≈ 0.40998
+        @test round(sourcemelt.vol/source.vol * 100,digits=2) ≈ 6.89
+        # @test sourcemelt.composition .≈ melt.composition
+
+        melth2o = getchemical(melt.composition,"H2O")
+        @test round(melth2o.μ,sigdigits=6) ≈ -316239
+        @test round(melth2o.mol,digits=3) ≈ 40.998
+        
+        hosth2o = getchemical(host.composition,"H2O")
+
+        @test round(hosth2o.mol,digits=3) ≈ 0.414
+        @test round(hosth2o.μ,sigdigits = 6) ≈ -316239
+        @test round(host.phases[1].vol/host.vol*100,digits=2) ≈ 5.15
+
+        h2ostart = 1.0
+        h2oend = 50.0
+        
+        source_compo1 = change_list_component(sourcecompo,h2ostart,"H2O")
+        source_compo2 = change_list_component(sourcecompo,h2oend,"H2O")
+
+        sourcecomporange = range(source_compo1,source_compo2,10)
+        @show sourcecomporange
+        systems = equilibrate_closed_system.(fill(sourcelib,10),sourcecomporange,fill(hostcompo,10),875,10000,800,9000,2.0)
+       
+        source = systems[10][1]
+        melt = systems[10][2]
+        host = systems[10][3]
+        sourcemelt = getmelt(source)
+        sourcemelth2o= getchemical(sourcemelt.composition,"H2O")
+        @test round(sourcemelth2o.mol,digits=5) ≈ 0.67574
+        @test round(sourcemelt.vol/source.vol * 100,digits=2) ≈ 97.78
+        # @test sourcemelt.composition .≈ melt.composition
+
+        melth2o = getchemical(melt.composition,"H2O")
+        @test round(melth2o.μ,sigdigits=6) ≈ -311522
+        @test round(melth2o.mol,digits=3) ≈ 67.574
+        
+        hosth2o = getchemical(host.composition,"H2O")
+
+        @test round(hosth2o.mol,digits=3) ≈ 47.143
+        @test round(hosth2o.μ,sigdigits=6) ≈ -311522
+        hostmelt = getmelt(host)
+        @test round(hostmelt.vol/host.vol*100,digits=2) ≈ 96.07
+        hostmelth2o = getchemical(hostmelt.composition,"H2O")
+        @test round(hostmelth2o.mol,digits=2) ≈ 0.67
+
+        hosts = [sys[3] for sys in systems]
+        fig = Figure(size = (600,450))
+        ax = Axis(fig[1,1])
+        phasemode!(ax,range(h2ostart,h2oend,10),hosts)
+        fig[1,2] = Legend(fig,ax)
+        save("23SD20A_melt-test3/Host.svg",fig)
+
+        close_meemum!(sourcelib)
+    end
+
     
-    hosth2o = getchemical(host.composition,"H2O")
-
-    @test round(hosth2o.mol,digits=3) ≈ 47.143
-    @test round(hosth2o.μ,sigdigits=6) ≈ -311522
-    hostmelt = getmelt(host)
-    @test round(hostmelt.vol/host.vol*100,digits=2) ≈ 96.07
-    hostmelth2o = getchemical(hostmelt.composition,"H2O")
-    @test round(hostmelth2o.mol,digits=2) ≈ 0.67
-
-
-
-    sourcelib = init_meemum("23SD20A_melt-test1/MeltSource")
-    source_compo = getcompo(sourcelib)
-    close_meemum!(sourcelib)
-
-    h2ostart = 1.0
-    h2oend = 50.0
-    
-    source_compo1 = change_list_component(source_compo,h2ostart,"H2O")
-    source_compo2 = change_list_component(source_compo,h2oend,"H2O")
-   
-    sources, melts, hosts = equilibrate_open_system("23SD20A_melt-test1/MeltSource","23SD20A_melt-test1/Host",875,10000,800,9000,source_compo1,source_compo2, steps =10)
-
-    source = sources[1]
-    melt = melts[1]
-    host = hosts[1]
-    sourcemelt = getmelt(source)
-    sourcemelth2o= getchemical(sourcemelt.composition,"H2O")
-    @test round(sourcemelth2o.mol,digits=5) ≈ 0.40998
-    @test round(sourcemelt.vol/source.vol * 100,digits=2) ≈ 6.89
-    # @test sourcemelt.composition .≈ melt.composition
-
-    melth2o = getchemical(melt.composition,"H2O")
-    @test round(melth2o.μ,sigdigits=6) ≈ -316239
-    @test round(melth2o.mol,digits=3) ≈ 40.998
-    
-    hosth2o = getchemical(host.composition,"H2O")
-
-    @test round(hosth2o.mol,digits=3) ≈ 0.414
-    @test round(hosth2o.μ,sigdigits = 6) ≈ -316239
-    @test round(host.phases[1].vol/host.vol*100,digits=2) ≈ 5.15
-
-    source = sources[10]
-    melt = melts[10]
-    host = hosts[10]
-    sourcemelt = getmelt(source)
-    sourcemelth2o= getchemical(sourcemelt.composition,"H2O")
-    @test round(sourcemelth2o.mol,digits=5) ≈ 0.67574
-    @test round(sourcemelt.vol/source.vol * 100,digits=2) ≈ 97.78
-    # @test sourcemelt.composition .≈ melt.composition
-
-    melth2o = getchemical(melt.composition,"H2O")
-    @test round(melth2o.μ,sigdigits=6) ≈ -311522
-    @test round(melth2o.mol,digits=3) ≈ 67.574
-    
-    hosth2o = getchemical(host.composition,"H2O")
-
-    @test round(hosth2o.mol,digits=3) ≈ 47.143
-    @test round(hosth2o.μ,sigdigits=6) ≈ -311522
-    hostmelt = getmelt(host)
-    @test round(hostmelt.vol/host.vol*100,digits=2) ≈ 96.07
-    hostmelth2o = getchemical(hostmelt.composition,"H2O")
-    @test round(hostmelth2o.mol,digits=2) ≈ 0.67
-
-
-
-    fig = Figure(size = (600,450))
-    ax = Axis(fig[1,1])
-    phasemode!(ax,range(h2ostart,h2oend,10),hosts)
-    fig[1,2] = Legend(fig,ax)
-    save("23SD20A_melt-test1/Host.svg",fig)
-    # sourcelib = init_meemum("23SD20A_melt-test1/MeltSource")
-    # source_compo = getcompo(sourcelib)
-    # close_meemum!(sourcelib)
-
-    # T_start = 850
-    # T_end = 950
-    # sources, melts, hosts = equilibrate_open_system("23SD20A_melt-test1/MeltSource","23SD20A_melt-test1/Host",T_start,T_end,10000,800,9000)
-    
-    # host_melt_percent = Float64[]
-    # hostH2O = Float64[]
-    # for host in hosts 
-    #     host_melt = getmelt(host)
-    #     push!(host_melt_percent,host_melt.vol/host.vol*100)
-    #     h2oindex = findchemical(host.composition,"H2O")
-    #     if h2oindex > 0
-    #         push!(hostH2O,concentration(host.composition[h2oindex]))
-    #     else
-    #         push!(hostH2O,0)
-    #     end
-    # end
-
-    # @show range(h2ostart,h2oend,length=100)
-    # # @show range(T_start,T_end,length=100)
-    # @show host_melt_percent
-    # @show hostH2O
-
-    # meltH2O = Float64[]
-    # meltuh2o = Float64[]
-    
-    # for melt in melts
-    #     h2oindex = findchemical(melt.composition,"H2O")
-    #     if h2oindex > 0
-    #         push!(meltH2O,concentration(melt.composition[h2oindex]))
-    #         push!(meltuh2o,melt.composition[h2oindex].μ)
-    #     else
-    #         push!(meltH2O,0)
-    #         push!(meltuh2o,0)
-    #     end
-    # end
-
-    # @show meltH2O
-    # @show meltuh2o
-
-    # source_melt_percent = Float64[]
-    # sourceH2O = Float64[]
-    # for source in sources
-    #     source_melt = getmelt(source)
-    #     push!(source_melt_percent, source_melt.vol/source.vol*100)
-    #     h2oindex = findchemical(source.composition,"H2O")
-    #     if h2oindex > 0
-    #         push!(sourceH2O,concentration(source.composition[h2oindex]))
-    #     else
-    #         push!(sourceH2O,0)
-    #     end
-    # end
-
-   
-    # @show source_melt_percent
-    # @show sourceH2O
 end
